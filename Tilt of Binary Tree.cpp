de2515300411b350208dd4a298d1d38a31ff1cb0
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-15 00:56:32 +0530

    Add files via upload

---
+++ Tilt of Binary Tree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+int sum(Node *root)
+{
+ if(root==NULL)
+  return 0;
+ return sum(root->left)+root->val+sum(root->right);
+}
+int tiltTree(Node* root)
+{
+
+ if(root==NULL)
+  return 0;
+
+ int LS=sum(root->left);
+ int RS=sum(root->right);
+ return abs(LS-RS)+tiltTree(root->left)+tiltTree(root->right);
+
+}
