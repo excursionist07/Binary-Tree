831411db7ef3032ad3165c62b5a0f282ba35c22c
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-16 00:46:40 +0530

    Update Check if two Nodes are Cousins.cpp

--- Check if two Nodes are Cousins.cpp
+++ Check if two Nodes are Cousins.cpp
@@ -1,31 +1,8 @@
-void makeparents(Node *root,int parent[])
-{
- parent[root->data]=-1;
- queue<Node*>q;
- q.push(root);
- while(!q.empty())
- {
-  Node *cur=q.front();
-  q.pop();
-  if(cur->left)
-  {
-    parent[cur->left->data]=cur->data;
-    q.push(cur->left);
-  }
-  if(cur->right)
-  {
-    parent[cur->right->data]=cur->data;
-    q.push(cur->right);
-  }
- }
-
-}
 bool isCousins(Node *root, int x, int y)
 {
  if(root==NULL || x==y)
-  return 0;
- int parent[1007]={0};
- makeparents(root,parent);
+  return true;
+
  queue<Node*>q;
  q.push(root);
  while(!q.empty())
@@ -36,7 +13,9 @@ bool isCousins(Node *root, int x, int y)
    {
      Node* cur=q.front();
      q.pop();
-     if(cur->data==x || cur->data==y)
+     if(cur->left && (cur->left->data==x || cur->left->data==y))
+      cnt++;
+     else if(cur->right  && (cur->right->data==x || cur->right->data==y))
       cnt++;
      if(cur->left)
       q.push(cur->left);
@@ -44,14 +23,8 @@ bool isCousins(Node *root, int x, int y)
       q.push(cur->right);
    }
    if(cnt==2)
-   {
-     if(parent[x]!=parent[y])
-      return true;
-     else
-      return false;
-   }
-   else if(cnt==1)
-    return false;
+    return true;
+
  }
- //return false;
+ return false;
 }
