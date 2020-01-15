e1e85b9b62c46268f71dfd4d0644f8c4dc2f2172
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-15 19:50:15 +0530

    Update Determine if Two Trees are Identical.cpp

--- Determine if Two Trees are Identical.cpp
+++ Determine if Two Trees are Identical.cpp
@@ -15,51 +15,16 @@ typedef long long int ll;
 ll HRX=1e18;
 ll INF=1e9+7;
 
-vector<int>v1,v2;
-void in1(Node *r1)
+bool Identical(Node *root1,Node *root2)
 {
-  if(r1==NULL)
-    return;
-  in1(r1->left);
-  v1.push_back(r1->data);
-  in1(r1->right);
-
-}
-void in2(Node *r2)
-{
-  if(r2==NULL)
-    return;
-  in2(r2->left);
-  v2.push_back(r2->data);
-  in2(r2->right);
-
+ if(!root1 && !root2)
+  return true;
+ if(root1 && root2 && root1->data==root2->data)
+  return Identical(root1->left,root2->left) && Identical(root1->right,root2->right);
+  
+ return false;
 }
 bool isIdentical(Node *r1, Node *r2)
 {
-  v1.clear();
-  v2.clear();
-  if(r1==NULL && r2==NULL)
-    return 1;
-  in1(r1);
-  in2(r2);
-  int ff=0;
-  if(v1.size()!=v2.size())
-    return 0;
-  else
-  {
-    for(int i=0;i<v1.size();i++)
-    {
-      if(v1[i]==v2[i])
-        continue;
-      else
-      {
-         ff=1;
-         break;
-      }
-    }
-    if(ff==0)
-      return 1;
-    else
-      return 0;
-  }
+    return Identical(r1,r2);
 }
