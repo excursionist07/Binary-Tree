354de3e978748d8b08d53871a056925b18428fcd
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-15 17:15:26 +0530

    Add files via upload

---
+++ Maximum Width of Tree.cpp
@@ -0,0 +1,23 @@
+int getMaxWidth(Node* root)
+{
+  if(root==NULL)
+   return 0;
+  queue<Node*>q;
+  q.push(root);
+  int maxx=INT_MIN;
+  while(!q.empty())
+  {
+    int zz=q.size();
+    maxx=max(maxx,zz);
+    for(int i=0;i<zz;i++)
+    {
+     Node *cur=q.front();
+     q.pop();
+     if(cur->left)
+      q.push(cur->left);
+     if(cur->right)
+      q.push(cur->right);
+    }
+  }
+  return maxx;
+}
