209c51a96ec435748ad16c348d62cb964bc8721b
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2020-01-19 00:37:38 +0530

    Add files via upload

---
+++ Leaves to DLL.cpp
@@ -0,0 +1,26 @@
+
+
+Node *convertToDLL(Node *root,Node **head_ref)
+{
+ if(!root)
+  return NULL;
+ if(!root->left && !root->right)
+ {
+   if(*head_ref==NULL)
+    *head_ref=root;
+   else
+   {
+     root->right=*head_ref;
+     (*head_ref)->left=root;
+     *head_ref=root;
+
+   }
+   return NULL;
+ }
+
+  root->right=convertToDLL(root->right,head_ref);
+
+  root->left=convertToDLL(root->left,head_ref);
+
+ //return *head_ref;
+}
