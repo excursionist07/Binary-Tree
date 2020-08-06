87ede0c3989e7b19b5be62be46fb77a587e86738
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-07 03:35:11 +0530

    Update Vertical sum.cpp

--- Vertical sum.cpp
+++ Vertical sum.cpp
@@ -21,19 +21,20 @@ Node * printVerticalUtil(Node *root,int hd,map<int,int>&mp)
  if(root==NULL)
   return NULL;
  printVerticalUtil(root->left,hd-1,mp);
- mp[hd]+=root->data;
  printVerticalUtil(root->right,hd+1,mp);
+ mp[hd]+=root->data;
 
 }
-void printVertical(Node *root)
+vector <int> verticalSum(Node *root) 
 {
+  vector<int>vv;
   map<int,int>mp;
   printVerticalUtil(root,0,mp);
   map<int,int>::iterator itr;
   for(itr=mp.begin();itr!=mp.end();++itr)
-  {
-    cout<<itr->second<<" ";
-  }
+    vv.push_back(itr->second);
+  return vv;
+ 
 }
 /*
 Given a Binary Tree, find the vertical sum of the nodes that are in the same vertical line. Print all sums through different vertical lines.
