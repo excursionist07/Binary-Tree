d569a131bf57a788abefb115016d29abfddc9aa0
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-07 03:40:32 +0530

    Update Diagonalsum.cpp

--- Diagonalsum.cpp
+++ Diagonalsum.cpp
@@ -1,36 +1,22 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-Node *diagonalSumUtil(Node *root,int dd,map<int,int>&mp)
+void diagonalSumUtil(Node *root,int dd,map<int,int>&mp)
 {
  if(root==NULL)
     return;
- mp[dd]+=root->data;
+
  diagonalSumUtil(root->left,dd+1,mp);
  diagonalSumUtil(root->right,dd,mp);
+  mp[dd]+=root->data;
 }
 
-void diagonalSum(Node *root)
+
+vector <int> diagonalSum(Node* root)
 {
- map<int,int>mp;
+  vector<int>vv;
+  map<int,int>mp;
  diagonalSumUtil(root,0,mp);
 
  map<int,int>::iterator itr;
  for(itr=mp.begin();itr!=mp.end();++itr)
-    cout<<itr->second<<" ";
-
+    vv.push_back(itr->second);
+ return vv;
 }
