8dc99f828f37f235aa0da3ea61c249ed12f953f2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 18:49:49 +0530

    Update topview.cpp

--- topview.cpp
+++ topview.cpp
@@ -1,47 +1,35 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
 void topView(struct Node *root)
 {
-  if(root==NULL)
+  if(!root)
    return;
-  int hd=0;
   map<int,int>mp;
   queue<pair<Node*,int>>q;
+  int hd=0;
   q.push(make_pair(root,hd));
   while(!q.empty())
   {
     pair<Node*,int>temp=q.front();
     q.pop();
-    Node *cur=temp.first;
+    Node* cur=temp.first;
     hd=temp.second;
     if(mp.count(hd)==0)
       mp[hd]=cur->data;
-    if(cur->left!=NULL)
+    if(cur->left)
       q.push(make_pair(cur->left,hd-1));
-    if(cur->right!=NULL)
-      q.push(make_pair(cur->right,hd+1));
+    if(cur->right)
+       q.push(make_pair(cur->right,hd+1));
+     
   }
-  map<int,int>::iterator itr;
-  for(itr=mp.begin();itr!=mp.end();++itr)
-    cout<<itr->second<<" ";
+  for(auto xx:mp)
+   cout<<xx.second<<" ";
 }
+
+
+
 
 /*
 A node x is there in output if x is the topmost node at its horizontal distance. Horizontal distance of left child of a node x is equal to horizontal distance of x minus 1, 
 and that of right child is horizontal distance of x plus 1.
+
+If there are multiple top-most nodes for a horizontal distance from root, then print the first one in level traversal.
 */
