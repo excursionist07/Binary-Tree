f1bec83e98aaa8a07d1f53829965ccdc0682b59e
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 18:43:22 +0530

    Update bottomview.cpp

--- bottomview.cpp
+++ bottomview.cpp
@@ -1,49 +1,29 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-void bottomView(Node *root)
+vector <int> bottomView(Node *root)
 {
-  if(root==NULL)
-   return;
-  int hd=0;
+  vector<int>ans;
+  if(!root)
+   return ans;
   map<int,int>mp;
   queue<pair<Node*,int>>q;
+  int hd=0;
   q.push(make_pair(root,hd));
-
   while(!q.empty())
   {
     pair<Node*,int>temp=q.front();
     q.pop();
-    Node *cur=temp.first;
-     hd=temp.second;
+    Node* cur=temp.first;
+    hd=temp.second;
     mp[hd]=cur->data;
-    if(cur->left!=NULL)
-    {
-     q.push(make_pair(cur->left,hd-1));
-    }
-    if(cur->right!=NULL)
-    {
-     q.push(make_pair(cur->right,hd+1));
-    }
+    if(cur->left)
+      q.push(make_pair(cur->left,hd-1));
+    if(cur->right)
+       q.push(make_pair(cur->right,hd+1));
   }
-  map<int,int>::iterator itr;
-  for(itr=mp.begin();itr!=mp.end();++itr)
-    cout<<itr->second<<" ";
+  for(auto xx:mp)
+   ans.push_back(xx.second);
+  return ans;
 }
+
 
 /*
 A node x is there in output if x is the bottommost node at its horizontal distance. Horizontal distance of left child of a node x is equal to horizontal distance of x minus 1, 
