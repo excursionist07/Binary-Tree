ec077a61ea3c2d099b50585535d737621cc34092
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 17:10:35 +0530

    Update Tilt of Binary Tree.cpp

--- Tilt of Binary Tree.cpp
+++ Tilt of Binary Tree.cpp
@@ -3,20 +3,22 @@ The tilt of a tree node is defined as the absolute difference between the sum of
 The tilt of the whole tree is defined as the sum of all nodes' tilt.----VERY IMPORTANT,it should not be misunderstood just to find abs(LS-RS) ONLY.
 */
 
-int sum(Node *root)
-{
- if(root==NULL)
-  return 0;
- return sum(root->left)+root->val+sum(root->right);
-}
-int tiltTree(Node* root)
-{
-
- if(root==NULL)
-  return 0;
-
- int LS=sum(root->left);
- int RS=sum(root->right);
- return abs(LS-RS)+tiltTree(root->left)+tiltTree(root->right);
-
-}
+class Solution {
+public:
+    int sum(TreeNode* root)
+    {
+     if(!root)
+         return 0;
+     return sum(root->left)+root->val+sum(root->right);
+    }
+    int findTilt(TreeNode* root) 
+    {
+     if(!root)
+         return 0;
+     int Lsum=sum(root->left);
+     int Rsum=sum(root->right);
+     
+     return abs(Lsum-Rsum)+findTilt(root->left)+findTilt(root->right);
+     
+    }
+};
