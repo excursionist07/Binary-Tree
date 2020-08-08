195349a26e5acce33f277c5ced70fa1a094794e2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 16:10:58 +0530

    Update Invert Tree.cpp

--- Mirror Tree/Invert Tree.cpp
+++ Mirror Tree/Invert Tree.cpp
@@ -1,32 +1,27 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-void mirror(Node* node)
-{
-  if(node==NULL)
-   return;
-  if(node->left)
-   mirror(node->left);
-  if(node->right)
-   mirror(node->right);
-
-  Node *temp=node->left;
-  node->left=node->right;
-  node->right=temp;
-
-
-}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) 
+    {
+      if(!root)
+          return NULL;
+      if(root->left)
+          invertTree(root->left);
+      if(root->right)
+          invertTree(root->right);
+      TreeNode* temp=root->left;
+      root->left=root->right;
+      root->right=temp;
+      return root;
+    }
+};
