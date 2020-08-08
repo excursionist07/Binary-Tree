e4a06b01b4013710c26ea0be92eb60062f641d70
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 15:16:10 +0530

    Update Count Leaves in Binary Tree.cpp

--- Count Leaves in Binary Tree.cpp
+++ Count Leaves in Binary Tree.cpp
@@ -1,26 +1,8 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
 int countLeaves(Node* root)
 {
-  if(root==NULL)
-    return 0;
-  else if(root->left==NULL && root->right==NULL)
-   return 1;
-  else
-    return countLeaves(root->left)+countLeaves(root->right);
+ if(root==NULL)
+  return 0;
+ else if(!root->left && !root->right)
+  return 1;
+ return countLeaves(root->left)+countLeaves(root->right);
 }
