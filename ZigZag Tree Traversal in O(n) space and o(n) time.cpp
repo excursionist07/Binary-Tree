dae74a35ca9bc72f5d28a1088c50689f96fd3df1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 13:56:28 +0530

    Update ZigZag Tree Traversal in O(n) space and o(n) time.cpp

--- ZigZag Tree Traversal in O(n) space and o(n) time.cpp
+++ ZigZag Tree Traversal in O(n) space and o(n) time.cpp
@@ -1,49 +1,54 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-void zigZagTraversal(Node* root)
-{
-  stack<Node*>s1;
-  stack<Node*>s2;
-  s1.push(root);
-  while(!s1.empty() || !s2.empty())
-  {
-    while(!s1.empty())
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
     {
-     Node *temp=s1.top();
-     s1.pop();
-     cout<<temp->data<<" ";
-     if(temp->left)
-       s2.push(temp->left);
-     if(temp->right)
-       s2.push(temp->right);
-
+     vector<vector<int>>ans;
+     if(!root)
+         return ans;
+     stack<TreeNode*>s1,s2;
+     s1.push(root);
+     while(!s1.empty() || !s2.empty())
+     {
+      vector<int>vv;
+      while(!s1.empty())
+      {
+       TreeNode* cur=s1.top();
+       s1.pop();
+       vv.push_back(cur->val);
+        if(cur->left)
+           s2.push(cur->left);
+       if(cur->right)
+           s2.push(cur->right);
+      
+      }
+      if(vv.size()>0)
+       ans.push_back(vv);
+      vv.clear();
+      while(!s2.empty())
+      {
+       TreeNode* cur=s2.top();
+       s2.pop();
+       vv.push_back(cur->val);
+       if(cur->right)
+           s1.push(cur->right);
+       if(cur->left)
+           s1.push(cur->left);
+      }
+      if(vv.size()>0)
+       ans.push_back(vv);
+     }
+     return ans;
+        
     }
-    while(!s2.empty())
-    {
-     Node *temp=s2.top();
-     s2.pop();
-     cout<<temp->data<<" ";
-
-     if(temp->right)
-       s1.push(temp->right);
-     if(temp->left)
-       s1.push(temp->left);
-    }
-  }
-  cout<<"\n";
-}
+};
