7d8387dec3220dfe35fafdfe13498f5f08d4d269
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 01:31:21 +0530

    Update Kth Ancestor in a Tree.cpp

--- Kth Ancestor in a Tree.cpp
+++ Kth Ancestor in a Tree.cpp
@@ -1,11 +1,11 @@
-void makeparent(Node *root,int parent[])
+void makeparent(Node* root,int parent[])
 {
  parent[root->data]=-1;
  queue<Node*>q;
  q.push(root);
  while(!q.empty())
  {
-  Node *cur=q.front();
+  Node* cur=q.front();
   q.pop();
   if(cur->left)
   {
@@ -14,26 +14,26 @@ void makeparent(Node *root,int parent[])
   }
   if(cur->right)
   {
-   parent[cur->right->data]=cur->data;
-   q.push(cur->right);
+    parent[cur->right->data]=cur->data;
+    q.push(cur->right);
   }
-
  }
 }
-
 int kthAncestor(Node *root, int k, int node)
 {
- if(k==0 || !root)
+  if(!root)
+   return -1;
+  if(k==0)
+   return node;
+  int parent[10007];
+  makeparent(root,parent);
+  int cnt=0;
+  while(node!=-1)
+  {
+    node=parent[node];
+    cnt++;
+    if(cnt==k)
+     return node;
+  }
   return -1;
- int parent[10204];
- makeparent(root,parent);
- int cnt=0;
- while(node!=-1)
- {
-   node=parent[node];
-   cnt++;
-   if(cnt==k)
-    break;
- }
- return node;
 }
