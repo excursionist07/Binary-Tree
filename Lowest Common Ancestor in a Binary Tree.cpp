a929a9bb5d76999cc1b3037e281eb4fd43d00584
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 01:01:55 +0530

    Update Lowest Common Ancestor in a Binary Tree.cpp

--- Lowest Common Ancestor in a Binary Tree.cpp
+++ Lowest Common Ancestor in a Binary Tree.cpp
@@ -5,17 +5,19 @@ Following is simple O(n) algorithm to find LCA of n1 and n2.
 3) Traverse both paths till the values in arrays are same. Return the common element just before the mismatch.
 
   
-Node* lca(Node* root,int a,int b) //O(n) && O(1)
-{
- if(root==NULL)
-  return NULL;
- if(root->data==a || root->data==b)
-   return root;
- Node* l=lca(root->left,a,b);
- Node* r=lca(root->right,a,b);
- if(l && r)return root;
- if(!l && !r)return NULL;
- if(l) return l;
- if(r) return r;
-  
-}
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) //o(n) && O(1)
+    {
+     if(!root)
+         return NULL;
+    if(root->val==p->val || root->val==q->val)
+        return root;
+    TreeNode* l=lowestCommonAncestor(root->left,p,q);
+    TreeNode* r=lowestCommonAncestor(root->right,p,q);
+    if(l && r) return root;
+    if(l) return l;
+    if(r) return r;
+    return NULL;
+    }
+};
