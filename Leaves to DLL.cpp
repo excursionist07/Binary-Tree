4d8e87a171111a00fbdbb6a24ee3e5aee2ab30aa
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 20:25:52 +0530

    Update Leaves to DLL.cpp

--- Leaves to DLL.cpp
+++ Leaves to DLL.cpp
@@ -1,26 +1,27 @@
-
-
-Node *convertToDLL(Node *root,Node **head_ref)
+Node* doit(Node* root,Node* &head)
 {
  if(!root)
   return NULL;
  if(!root->left && !root->right)
  {
-   if(*head_ref==NULL)
-    *head_ref=root;
-   else
+  if(!head)
+   head=root;
+  else
    {
-     root->right=*head_ref;
-     (*head_ref)->left=root;
-     *head_ref=root;
-
+    root->right=head;
+    head->left=root;
+    head=root;
    }
-   return NULL; // this statement detaches the leaf from the tree
+   return NULL;// this statement detaches the leaf from the tree
  }
-
-  root->right=convertToDLL(root->right,head_ref);
-
-  root->left=convertToDLL(root->left,head_ref);
-
- //return *head_ref;
+ root->right=doit(root->right,head);
+ root->left=doit(root->left,head);
+}
+Node * convertToDLL(Node *root)
+{
+  if(!root)
+   return NULL;
+  Node* head=NULL;
+  doit(root,head);
+  return head;
 }
