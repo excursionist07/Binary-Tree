e075e9bee65e6d67fffe47e854298fa5ebf4831a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 18:15:38 +0530

    Update Check for Balanced Tree.cpp

--- Check for Balanced Tree.cpp
+++ Check for Balanced Tree.cpp
@@ -1,50 +1,25 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-
-int height(Node *root)
-{
-  if(root==NULL)
-   return -1;
-  int LH=height(root->left);
-  int RH=height(root->right);
-  return 1+max(LH,RH);
-
-}
-
-  bool isBalanced(node* root) 
-{ 
-   
-  
-    /* If tree is empty then return true */
-    if (root == NULL) 
-        return 1; 
-  
-    /* Get the height of left and right sub trees */
-    int lh = height(root->left); 
-    int rh = height(root->right); 
-  
-    if (abs(lh - rh) <= 1 && isBalanced(root->left) && isBalanced(root->right)) 
-        return 1; 
-  
-    /* If we reach here then  
-    tree is not height-balanced */
-    return 0; 
-} 
-
-// a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
-
+class Solution {
+public:
+    int height(TreeNode* root)
+    {
+     if(!root)
+         return -1;
+     else
+     {
+      int LH=height(root->left);
+      int RH=height(root->right);
+      return 1+max(LH,RH);
+     }
+    }
+    bool isBalanced(TreeNode* root)
+    {
+      if(!root)
+          return 1;
+      int L=height(root->left);
+      int R=height(root->right);
+      
+      if(abs(L-R)<=1 && isBalanced(root->left) && isBalanced(root->right))
+          return 1;
+     return 0;
+    }
+};
