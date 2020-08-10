7167ce74ac913e3d894af7e4103150244cb398a1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 21:20:54 +0530

    Update Check if two Nodes are Cousins.cpp

--- Check if two Nodes are Cousins.cpp
+++ Check if two Nodes are Cousins.cpp
@@ -1,30 +1,28 @@
-bool isCousins(Node *root, int x, int y)
+bool isCousins(Node *root, int a, int b)
 {
- if(root==NULL || x==y)
-  return true;
-
- queue<Node*>q;
- q.push(root);
- while(!q.empty())
- {
-   int zz=q.size();
-   int cnt=0;
+  if(!root || a==b)
+   return true;
+  queue<Node*>q;
+  q.push(root);
+  while(!q.empty())
+  {
+   int cnt=0,zz=q.size();
    for(int i=0;i<zz;i++)
    {
-     Node* cur=q.front();
-     q.pop();
-     if(cur->left && (cur->left->data==x || cur->left->data==y))
-      cnt++;
-     else if(cur->right  && (cur->right->data==x || cur->right->data==y))
-      cnt++;
-     if(cur->left)
-      q.push(cur->left);
-     if(cur->right)
-      q.push(cur->right);
+    Node* cur=q.front();
+    q.pop(); 
+    if(cur->left && (cur->left->data==a || cur->left->data==b))
+     cnt++;
+    else if(cur->right && (cur->right->data==a || cur->right->data==b))
+     cnt++;
+    if(cur->left)
+     q.push(cur->left);
+    if(cur->right)
+     q.push(cur->right);
+   
    }
-   if(cnt==2)
-    return true;
-
- }
- return false;
+    if(cnt==2)
+     return true;
+  }
+  return false;
 }
