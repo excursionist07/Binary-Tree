840a16865f2e5e16541f932cb8331d138ac1c175
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 14:58:20 +0530

    Update Odd even level difference.cpp

--- Odd even level difference.cpp
+++ Odd even level difference.cpp
@@ -1,45 +1,32 @@
 int getLevelDiff(Node *root)
 {
-  int even=0,odd=0,ff=0;
+  if(!root)
+   return 0;
+  int odd=0,even=0;
   queue<Node*>q;
   q.push(root);
+  bool ff=1;
   while(!q.empty())
   {
-    int zz=q.size();
-    if(ff==0)
-    {
-     for(int i=0;i<zz;i++)
-     {
-      Node *cur=q.front();
-      q.pop();
-      odd+=(cur->data);
-      if(cur->left)
-        q.push(cur->left);
-      if(cur->right)
-        q.push(cur->right);
-     }
-     ff=1;
-    }
-    else if(ff==1)
-    {
-     for(int i=0;i<zz;i++)
-     {
-      Node *cur=q.front();
-      q.pop();
-      even+=cur->data;
+   int n=q.size();
+   int sum=0;
+   for(int i=0;i<n;i++)
+   {
+     Node* cur=q.front();
+     q.pop();
+     sum+=cur->data;
      if(cur->left)
-        q.push(cur->left);
-      if(cur->right)
-        q.push(cur->right);
-     }
-     ff=0;
-    }
+      q.push(cur->left);
+     if(cur->right)
+      q.push(cur->right);
+   }
+   if(ff)
+    odd+=sum;
+   else
+    even+=sum;
+   ff=!ff;
   }
-  return (odd-even);
-
-  // SECOND METHOD
- /* if(root==NULL)
-   return 0;
-  return root->data-getLevelDiff(root->left)-getLevelDiff(root->right);*/
+  return odd-even;
+  
 }
 
