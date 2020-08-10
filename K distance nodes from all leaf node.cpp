54e06dbd9f29cbb4d118d1d424a1e6e8769f5a7c
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 13:29:29 +0530

    Update K distance nodes from all leaf node.cpp

--- K distance nodes from all leaf node.cpp
+++ K distance nodes from all leaf node.cpp
@@ -1,19 +1,19 @@
-set<Node*>s;
-void countNode(Node* root, int k,vector<Node*>vec)
+void doit(Node* root,int k,unordered_set<Node*>&ss,vector<Node*>vv)
 {
  if(!root)
   return;
- vec.push_back(root);
- if(!root->left && !root->right && vec.size()>k)
-  s.insert(vec[vec.size()-k-1]);
- countNode(root->left,k,vec);
- countNode(root->right,k,vec);
+ vv.push_back(root);
+ if(!root->left && !root->right && vv.size()>k)
+  ss.insert(vv[vv.size()-k-1]);
+ doit(root->left,k,ss,vv);
+ doit(root->right,k,ss,vv);
 }
-int printKDistantfromLeaf(Node* node, int k)
+int printKDistantfromLeaf(Node* root, int k)
 {
- s.clear();
- vector<Node*>vec;
- countNode(node,k,vec);
- return s.size();
-
+ unordered_set<Node*>ss;
+ vector<Node*>vv;
+ if(!root)
+  return 0;
+ doit(root,k,ss,vv);
+ return ss.size();
 }
