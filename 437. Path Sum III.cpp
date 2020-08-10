61491401cb1292a82b3b9ede03ca9a26c1d9602f
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 16:52:41 +0530

    Update 437. Path Sum III.cpp

--- 437. Path Sum III.cpp
+++ 437. Path Sum III.cpp
@@ -1,3 +1,7 @@
+/* Find the number of paths that sum to a given value.
+   The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+*/
+
 class Solution {
 public:
     int ans=0;
