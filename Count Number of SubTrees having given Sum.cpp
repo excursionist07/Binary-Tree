21e51853696197f9560bb3b0cbb89dcd13e28398
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 18:09:12 +0530

    Update Count Number of SubTrees having given Sum.cpp

--- Count Number of SubTrees having given Sum.cpp
+++ Count Number of SubTrees having given Sum.cpp
@@ -1,34 +1,15 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-int sum(Node *root)
+int sum(Node* root)
 {
- if(root==NULL)
+ if(!root)
   return 0;
- return root->data+sum(root->left)+sum(root->right);
+ return sum(root->left)+root->data+sum(root->right);
 }
-int countSubtreesWithSumX(Node* root, int x)
+int countSubtreesWithSumX(Node* root, int X)
 {
-	if (!root)
-	  return 0;
-	if(sum(root)==x)
-	 return 1+countSubtreesWithSumX(root->left,x)+countSubtreesWithSumX(root->right,x);
-	else
-	  return countSubtreesWithSumX(root->left,x)+countSubtreesWithSumX(root->right,x);
-
-
+ if(!root)
+  return 0;
+ if(sum(root)==X)
+  return 1+countSubtreesWithSumX(root->left,X)+countSubtreesWithSumX(root->right,X);
+ else
+   return countSubtreesWithSumX(root->left,X)+countSubtreesWithSumX(root->right,X);
 }
