040ea78e36f2c36d07f301682d7f0df5f7b8ece6
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-11 02:41:41 +0530

    Add files via upload

---
+++ Construct Tree from Preorder and Postorder.cpp
@@ -0,0 +1,16 @@
+ */
+class Solution {
+public:
+    int preIndex=0,postIndex=0;
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post)
+    {
+        TreeNode* root = new TreeNode(pre[preIndex++]);
+        if (root->val != post[postIndex])
+            root->left = constructFromPrePost(pre, post);
+        if (root->val != post[postIndex])
+            root->right = constructFromPrePost(pre, post);
+        postIndex++;
+        return root;
+
+    }
+};
