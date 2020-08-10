ac0fddbdfa0791f90f16607bf1c1e0facc8683bb
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 13:34:20 +0530

    Update Shortest distance in infinite tree.cpp

--- Shortest distance in infinite tree.cpp
+++ Shortest distance in infinite tree.cpp
@@ -1,39 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define mem(dp,a) memset(dp,a,sizeof(dp))
 #define pb(x) push_back(x)
 #define m_p(x,y) make_pair(x,y)
 #define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
+#define per(i,b,a) for (ll i=b;i>=a;i--)
+#define all(v) (v).begin(),(v).end()
+#define ff first
+#define ss second
+#define pi acosl(-1.0l)
 #define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fixed(n) fixed<<setprecision(n)
 using namespace std;
 typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+typedef long double ld;
+ll ESP=1e18;
+ll FCB=1e9+7;
+ll dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
+/*
+ freopen("in.txt","r",stdin);
+ freopen("out.txt","w",stdout);
+ */
+
 
 int main()
 {
  hs;
  ll t;
  cin>>t;
- f(t)
+ while(t--)
  {
   ll x,y;
   cin>>x>>y;
   ll cnt=0;
   while(x!=y && x>0 && y>0)
   {
-    if(x>y)
-     x/=2;
-    else
-     y/=2;
-    cnt++;
+   if(x>y)
+    x/=2;
+   else
+    y/=2;
+   cnt++;
   }
-  cout<<cnt<<endl;
+  cout<<cnt<<"\n";
  }
  return 0;
 }
