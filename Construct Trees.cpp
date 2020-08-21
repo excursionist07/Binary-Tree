36113f67de9ebccf6c3af2db990ea06cb773bcc1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-21 14:59:53 +0530

    Update Construct Trees.cpp

--- Construct Trees.cpp
+++ Construct Trees.cpp
@@ -339,3 +339,33 @@ void flatten(TreeNode* root)
 
       }
 
+// Leaves to DLL(in place)
+	
+Node* doit(Node* root,Node* &head)
+{
+ if(!root)
+  return NULL;
+ if(!root->left && !root->right)
+ {
+  if(!head)
+   head=root;
+  else
+   {
+    root->right=head;
+    head->left=root;
+    head=root;
+   }
+   return NULL;// this statement detaches the leaf from the tree
+ }
+ root->right=doit(root->right,head);
+ root->left=doit(root->left,head);
+}
+Node * convertToDLL(Node *root)
+{
+  if(!root)
+   return NULL;
+  Node* head=NULL;
+  doit(root,head);
+  return head;
+}
+
