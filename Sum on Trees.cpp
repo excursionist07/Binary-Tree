3442c7d2f90ca52329e55cb5db35839e8259b8b3
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-21 14:51:50 +0530

    Update Sum on Trees.cpp

--- Sum on Trees.cpp
+++ Sum on Trees.cpp
@@ -158,3 +158,22 @@ void toSumTree(Node *node)
   
 }
 
+// Sum of all the numbers that are formed from root to leaf paths
+
+class Solution {
+public:
+    int doit(TreeNode* root,int val)
+    {
+     if(!root)
+         return 0;
+     val=val*10+root->val;
+     if(!root->left && !root->right)
+         return val;
+     return doit(root->left,val)+doit(root->right,val);
+    }
+    int sumNumbers(TreeNode* root) 
+    {
+     return doit(root,0);    
+    }
+};
+
