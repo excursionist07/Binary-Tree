266601c2d1e8b7659f13d3b9172f466dca197060
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-21 14:33:14 +0530

    Update and rename Top,bottom,left,right View.cpp to Traversal on trees.cpp

--- Traversal on trees.cpp
+++ Traversal on trees.cpp
@@ -298,3 +298,116 @@ vector <int> diagonalSum(Node* root)
  return vv;
 }
 
+// ZigZag Tree Traversal
+
+// USING TWO STACKS
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
+    {
+     vector<vector<int>>ans;
+     if(!root)
+         return ans;
+     stack<TreeNode*>s1,s2;
+     s1.push(root);
+     while(!s1.empty() || !s2.empty())
+     {
+      vector<int>vv;
+      while(!s1.empty())
+      {
+       TreeNode* cur=s1.top();
+       s1.pop();
+       vv.push_back(cur->val);
+        if(cur->left)
+           s2.push(cur->left);
+       if(cur->right)
+           s2.push(cur->right);
+      
+      }
+      if(vv.size()>0)
+       ans.push_back(vv);
+      vv.clear();
+      while(!s2.empty())
+      {
+       TreeNode* cur=s2.top();
+       s2.pop();
+       vv.push_back(cur->val);
+       if(cur->right)
+           s1.push(cur->right);
+       if(cur->left)
+           s1.push(cur->left);
+      }
+      if(vv.size()>0)
+       ans.push_back(vv);
+     }
+     return ans;
+        
+    }
+};
+
+// USING ONLY ONE QUEUE
+
+ queue<TreeNode*>q;
+     q.push(root);
+     bool ff=1;// ff=1(L-R) , ff=0(R-L)
+     while(!q.empty())
+     {
+       int zz=q.size();
+       vector<int>vv(zz);
+       for(int i=0;i<zz;i++)
+       {
+        TreeNode *cur=q.front();
+        q.pop();
+        if(ff)
+          vv[i]=cur->val;
+        else
+          vv[zz-i-1]=cur->val;
+        if(cur->left)
+            q.push(cur->left);     
+        if(cur->right)
+            q.push(cur->right);
+       }
+       ans.push_back(vv);
+       ff=!ff;
+      
+     }
+     return ans;
+        
+    }
+};
+
+// Spiral Traversal
+
+void printSpiral(Node *root)
+{
+
+  stack<Node*>s1;
+  stack<Node*>s2;
+  s1.push(root);
+  while(!s1.empty() || !s2.empty())
+  {
+    while(!s1.empty())
+    {
+     Node *temp=s1.top();
+     s1.pop();
+     cout<<temp->data<<" ";
+     if(temp->right)
+       s2.push(temp->right);
+     if(temp->left)
+       s2.push(temp->left);
+    }
+    while(!s2.empty())
+    {
+     Node *temp=s2.top();
+     s2.pop();
+     cout<<temp->data<<" ";
+     if(temp->left)
+       s1.push(temp->left);
+     if(temp->right)
+       s1.push(temp->right);
+    }
+  }
+}
+
+
+
