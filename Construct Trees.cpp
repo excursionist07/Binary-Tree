d7e968116e532b40dc4c1670429c0332341e9eff
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-21 14:58:02 +0530

    Update Construct Trees.cpp

--- Construct Trees.cpp
+++ Construct Trees.cpp
@@ -322,3 +322,20 @@ Node * bToDLL(Node *root)
  return head;
 }
 
+// 114.Flatten Binary Tree to Linked List
+
+void flatten(TreeNode* root)
+    {
+      if(!root)return ;
+      flatten(root->left);
+      flatten(root->right);
+      if(root->left)
+      {
+       TreeNode *rr=root->right;
+       root->right=root->left;
+       root->left=NULL;
+       while(root->right)root=root->right;
+       root->right=rr;
+
+      }
+
