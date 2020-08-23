9fcdb0066e73ca4d17d7fd8fcb50cffe350fb6cb
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-23 21:34:37 +0530

    Update Sum on Trees.cpp

--- Sum on Trees.cpp
+++ Sum on Trees.cpp
@@ -202,3 +202,35 @@ int maxPathSum(Node* root)
   return res;
 }
 
+// Count Number of SubTrees having given Sum
+
+int sum(Node* root)
+{
+ if(!root)
+  return 0;
+ return sum(root->left)+root->data+sum(root->right);
+}
+int countSubtreesWithSumX(Node* root, int X)
+{
+ if(!root)
+  return 0;
+ if(sum(root)==X)
+  return 1+countSubtreesWithSumX(root->left,X)+countSubtreesWithSumX(root->right,X);
+ else
+   return countSubtreesWithSumX(root->left,X)+countSubtreesWithSumX(root->right,X);
+}
+
+// 617. Merge Two Binary Trees
+
+TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2)
+    {
+      if(!t1)
+          return t2;
+      if(!t2)
+          return t1;
+      t1->val+=t2->val;
+      t1->left= mergeTrees(t1->left,t2->left);
+      t1->right= mergeTrees(t1->right,t2->right);
+      return t1;
+    }
+
