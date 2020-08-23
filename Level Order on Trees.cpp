b9bf44fb5ef09f8f555d06ef541b7bba8cf5ad79
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-23 21:33:03 +0530

    Update Level Order on Trees.cpp

--- Level Order on Trees.cpp
+++ Level Order on Trees.cpp
@@ -136,3 +136,38 @@ int getMaxWidth(Node* root)
   }
   return maxx;
 }
+
+// Odd even level difference
+
+int getLevelDiff(Node *root)
+{
+  if(!root)
+   return 0;
+  int odd=0,even=0;
+  queue<Node*>q;
+  q.push(root);
+  bool ff=1;
+  while(!q.empty())
+  {
+   int n=q.size();
+   int sum=0;
+   for(int i=0;i<n;i++)
+   {
+     Node* cur=q.front();
+     q.pop();
+     sum+=cur->data;
+     if(cur->left)
+      q.push(cur->left);
+     if(cur->right)
+      q.push(cur->right);
+   }
+   if(ff)
+    odd+=sum;
+   else
+    even+=sum;
+   ff=!ff;
+  }
+  return odd-even;
+  
+}
+
