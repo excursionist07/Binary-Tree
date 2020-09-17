802f9b8b3449b8d60ce22e183339d29823e52186
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-18 01:12:03 +0530

    Update Clone a Binary Tree.cpp

--- Clone a Binary Tree.cpp
+++ Clone a Binary Tree.cpp
@@ -9,3 +9,31 @@ Node* cloneTree(Node* tree)
   return temp;
   
 }
+
+// 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+
+
+class Solution {
+public:
+    TreeNode* ans;
+    void doit(TreeNode* original,TreeNode* cloned,TreeNode* target)
+    {
+     if(!original)
+        return;
+     if(original==target)
+     {
+      ans=cloned;
+      return;
+     }
+     doit(original->left,cloned->left,target);
+     doit(original->right,cloned->right,target);
+    }
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) 
+    {
+     if(!original)
+         return NULL;
+     doit(original,cloned,target);
+     return ans;
+     
+    }
+};
