ab894d6bcffcce44db11894260decd629fb94989
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-20 14:07:04 +0530

    Update 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp

--- 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -56,3 +56,34 @@ public:
      return cnt;
     }
 };
+
+// o(n) time and 0(1) space
+
+class Solution {
+public:
+    void dfs(TreeNode* root,int xx,int& ans) // O(n) && 0(1)
+    {
+     if(!root)
+         return;
+     xx=xx^(1<<root->val); // if a particular number occurence is even times then it will be nullified 
+     int cnt=__builtin_popcount(xx);// count number of set bits in cnt
+     if(!root->left && !root->right)
+     {
+      if(cnt<=1) // cnt==0--> for even sized palindrome and cnt==1 for odd sized palaindrome,for a odd sized palindrome only one number occurence should be odd and for even all number occurence should be even
+          ans++;
+      return;
+     }
+     dfs(root->left,xx,ans);
+     dfs(root->right,xx,ans);
+
+    }
+    
+    int pseudoPalindromicPaths (TreeNode* root) 
+    {
+     if(!root)
+         return 0;
+     int ans=0;
+     dfs(root,0,ans);
+     return ans;
+    }
+};
