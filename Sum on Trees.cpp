5432206344cf147360047a455c86b01b70d6038a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-04-12 20:53:11 +0530

    Update Sum on Trees.cpp

--- Sum on Trees.cpp
+++ Sum on Trees.cpp
@@ -230,15 +230,20 @@ int countSubtreesWithSumX(Node* root, int X)
 
 // 617. Merge Two Binary Trees
 
-TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2)
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) 
     {
-      if(!t1)
-          return t2;
-      if(!t2)
-          return t1;
-      t1->val+=t2->val;
-      t1->left= mergeTrees(t1->left,t2->left);
-      t1->right= mergeTrees(t1->right,t2->right);
-      return t1;
+     if(!root1 && !root2)
+         return NULL;
+     if(!root1 && root2)
+          return root2;
+     if(root1 && !root2)
+          return root1;
+     root1->val+=root2->val;
+     root1->left=mergeTrees(root1->left,root2->left);
+     root1->right=mergeTrees(root1->right,root2->right);
+     return root1;
+     
     }
-
+};
