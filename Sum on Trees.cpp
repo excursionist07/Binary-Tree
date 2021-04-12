25631e4570fe018c8e2905a826b5855e897180eb
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-04-12 20:46:23 +0530

    Update Sum on Trees.cpp

--- Sum on Trees.cpp
+++ Sum on Trees.cpp
@@ -126,20 +126,17 @@ public:
 
 int sum(Node* root)
 {
- if(!root)
-  return 0;
- return sum(root->left)+root->data+sum(root->right);
+     if(!root)
+      return 0;
+     return sum(root->left)+root->data+sum(root->right);
 }
 bool isSumTree(Node* root)
 {
- if(!root || (!root->left && !root->right))
-  return 1;
- int Lsum=sum(root->left);
- int Rsum=sum(root->right);
- if(Lsum+Rsum==root->data && isSumTree(root->left) && isSumTree(root->right))
-  return true;
- else
-  return false;
+     if(!root || (!root->left && !root->right))
+      return 1;
+     int L=sum(root->left);
+     int R=sum(root->right);
+     return (root->data==L+R && isSumTree(root->left) && isSumTree(root->right));
 }
 
 // Transform to Sum Tree
