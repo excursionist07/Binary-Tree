94bcacac8814904b23d139d357732ef92fd688c2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-06-10 14:07:08 +0530

    Update Serialize and Deserialize a Binary Tree.cpp

--- Serialize and Deserialize a Binary Tree.cpp
+++ Serialize and Deserialize a Binary Tree.cpp
@@ -122,3 +122,70 @@ public:
     }
 };
 
+// 297. Serialize and Deserialize Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    void preorder(TreeNode* root,string& str)
+    {
+     if(!root)
+     {
+      str+="#";
+      str+=",";
+      return;
+     }
+     str+=to_string(root->val)+",";
+     preorder(root->left,str);
+     preorder(root->right,str);
+    }
+    string serialize(TreeNode* root) 
+    {
+     if(!root)return "#";
+     string str="";
+     preorder(root,str);
+     return str; // (str =1,2,#,#,3,4,#,#,5,#,#,) foor Example 1.
+    }
+
+    // Decodes your encoded data to tree.
+    int go(string& data,int& pos)
+    {
+     pos=data.find(',');
+     int value=stoi(data.substr(0,pos));
+     return value;
+    }
+    
+    TreeNode* doit(string& data)
+    {
+     if(data[0]=='#') // it means NULL
+     {
+       if(data.length()>1)
+           data=data.substr(2); // eleminating '#' and ',' from data
+       return NULL;
+     }
+     int pos=0;
+     int value=go(data,pos);
+     data=data.substr(pos+1);
+     TreeNode* cur=new TreeNode(value);
+     cur->left=doit(data);
+     cur->right=doit(data);
+     return cur;
+    }
+    TreeNode* deserialize(string data) 
+    {
+     return doit(data);
+    }
+};
+
+
+
