80144fc748ba4c5695d82c92e576333bc9660ca2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-06-10 14:45:25 +0530

    Update Clone a Binary Tree.cpp

--- Clone a Binary Tree.cpp
+++ Clone a Binary Tree.cpp
@@ -1,14 +1,40 @@
-Node* cloneTree(Node* tree)
-{
-  if(!tree)
-   return NULL;
-  Node* temp=new Node(tree->data);
-  temp->random=tree->random;
-  temp->left=cloneTree(tree->left);
-  temp->right=cloneTree(tree->right);
-  return temp;
+// Clone a Binary Tree 
+
+class Solution{
+  public:
+    /* The function should clone the passed tree and return 
+       root of the cloned tree */
   
-}
+    Node* doit(Node* tree,unordered_map<Node*,Node*>&mp)
+    {
+     if(!tree)
+      return NULL;
+     Node* cur=new Node(tree->data);
+     mp[tree]=cur;
+     cur->left=doit(tree->left,mp);
+     cur->right=doit(tree->right,mp);
+     return cur;
+    }
+  
+    void RandomPointer(Node* tree,Node* clonedRoot,unordered_map<Node*,Node*>&mp)
+    {
+     if(!clonedRoot)
+       return;
+     clonedRoot->random=mp[tree->random];
+     RandomPointer(tree->left,clonedRoot->left,mp);
+     RandomPointer(tree->right,clonedRoot->right,mp);
+     
+    }
+  
+    Node* cloneTree(Node* tree)
+    {
+     if(!tree)return NULL;
+     unordered_map<Node*,Node*>mp;
+     Node* clonedRoot=doit(tree,mp);
+     RandomPointer(tree,clonedRoot,mp);
+     return clonedRoot;
+    }
+};
 
 // 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
 
