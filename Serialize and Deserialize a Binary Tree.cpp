ba44803cdd84c02c8842ae852db54351f2005c75
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-06-10 14:19:56 +0530

    Update Serialize and Deserialize a Binary Tree.cpp

--- Serialize and Deserialize a Binary Tree.cpp
+++ Serialize and Deserialize a Binary Tree.cpp
@@ -1,76 +1,57 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// gfg
 
-void pre(Node *root,vector<int> &A)
+class Solution
 {
- if(root==NULL)
- {
-  A.push_back(-1);
-  return;
- }
- A.push_back(root->data);
- pre(root->left,A);
- pre(root->right,A);
-}
-int in;
+    public:
+    //Function to serialize a tree and return a list containing nodes of tree.
+ 
+    void preorder(Node* root,vector<int>& vv)
+    {
+     if(!root)
+     {
+      vv.push_back(-1);
+      return;
+     }
+     vv.push_back(root->data);
+     preorder(root->left,vv);
+     preorder(root->right,vv);
+    }
+    
+    vector<int> serialize(Node *root) 
+    {
+      if(!root)return {};
+      vector<int>vv;
+      preorder(root,vv);
+      return vv;
+    }
+    
+    //Function to deserialize a list and construct the tree.
+ 
+    Node* doit(vector<int>& A,int& idx)
+    {
+     if(idx==A.size()-1 || A[idx]==-1)
+     {
+      idx++;
+      return NULL;
+     }
+     Node* cur=new Node(A[idx]);
+     idx++;
+     cur->left=doit(A,idx);
+     cur->right=doit(A,idx);
+     return cur;
+    }
+    Node * deSerialize(vector<int> &A)
+    {
+      if(A.size()==0)return NULL;
+      int idx=0;
+      return doit(A,idx);
+    }
 
-void serialize(Node *root,vector<int> &A)
-{
-  A.clear();
-   in=0;
-  pre(root,A);
-
-}
-
-Node* GetNewNode(int x)
-{
- Node* temp=new Node();
-  temp->data=x;
- temp->left=temp->right=NULL;
- return temp;
-}
-
-Node * deSerialize(vector<int> &A)
-{
-  if(in==A.size() || A[in]==-1)
-  {
-    in++;
-    return NULL;
-  }
-  Node *temp=GetNewNode(A[in]);
-  in++;
-  temp->left=deSerialize(A);
-  temp->right=deSerialize(A);
-
-
-  return temp;
-}
+};
 
 // 449. Serialize and Deserialize BST
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Codec {
 public:
 
@@ -124,15 +105,7 @@ public:
 
 // 297. Serialize and Deserialize Binary Tree
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Codec {
 public:
 
