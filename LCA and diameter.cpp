a900394f211c080ff278739f64464bf28bb623e1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-06-22 23:52:54 +0530

    Update LCA and diameter.cpp

--- LCA and diameter.cpp
+++ LCA and diameter.cpp
@@ -16,11 +16,13 @@ public:
 
 // LCA in a Binary Tree
 
+/*
 Method 1 (By Storing root to n1 and root to n2 paths):
 Following is simple O(n) algorithm to find LCA of n1 and n2.
 1) Find path from root to n1 and store it in a vector or array.
 2) Find path from root to n2 and store it in another vector or array.
 3) Traverse both paths till the values in arrays are same. Return the common element just before the mismatch.
+*/
 
   
 class Solution {
